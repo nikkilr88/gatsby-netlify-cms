cfbb1f78ab41d373f08dab43cffc92d71d0a6dd3
Nicole Sattler <nicole.sattler@live.com>
2019-07-24 16:21:06 -0500

    Set up test blog and query

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/blog/" } }
+        sort: { fields: [frontmatter___date], order: DESC }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              date
+            }
+            excerpt
+          }
+        }
+      }
+    }
+  `)
+
+  const posts = data.allMarkdownRemark.edges.map(edge => (
+    <div key={edge.node.id}>
+      <h2>{edge.node.frontmatter.title}</h2>
+      <p>{edge.node.frontmatter.date}</p>
+      <p>{edge.node.excerpt}</p>
+    </div>
+  ))
+
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <h1>Blog</h1>
+      {posts}
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default SecondPage
